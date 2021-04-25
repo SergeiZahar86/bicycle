c3745c29b68204440223eef6835d739ccd52f55e
Сергей Захаров <sergei86zahar@mail.ru>
2021-04-25 18:02:42 +0700

    1

--- #Source/js/main.js
+++ #Source/js/main.js
@@ -1,17 +1,20 @@
 
+$(document).ready(function (){
+	$('.wrapper').addClass('loaded');
 
-$('.wrapper').addClass('loaded');
+// Для меню бургер
+	$('.icon-menu').click(function(event) {
+		$(this).toggleClass('active');
+		$('.menu__body').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 
-$('.icon-menu').click(function(event) {
-	$(this).toggleClass('active');
-	$('.menu__body').toggleClass('active');
-	$('body').toggleClass('lock');
 });
 
 
 
 // применяется для фулскриновой заставки, начальной страницы, с картинкой (применяет дополнительный
-// стиль к ibg)
+// стиль к ibg), картинку теперь можно менять в html
 function ibg(){
 	$.each($('.ibg'), function(index, val) {
 		if($(this).find('img').length>0){
